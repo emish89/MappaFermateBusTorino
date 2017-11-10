e8f1c9e8d8d18f22afaeb4b78e8bdc90805a2110
Federico Ballarini <federico.ballarini.sc@gmail.com>
2017-11-10 12:34:47 +0100

    prova heroku

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/id/:id', function (req, res) {
 
 
 /** Start del server */
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8081, function () {
 
     var host = server.address().address
     var port = server.address().port
